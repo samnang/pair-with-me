ee658b8d9b8e5a61220c671cb315f2af706c7a6e
samnang <samnang.chhun@gmail.com>
2011-02-18 07:31:43 +0700

    refactor move back find_user_database_authentication out of private scope

--- app/models/user.rb
+++ app/models/user.rb
@@ -41,12 +41,12 @@ class User < ActiveRecord::Base
     username
   end
 
-  private
-
   def self.find_for_database_authentication(conditions)
     login = conditions.delete(:login)
     where(conditions).where(["username = :value OR email = :value", { :value => login }]).first
   end
+
+  private
   
   def set_full_name
     self.full_name = self.username
