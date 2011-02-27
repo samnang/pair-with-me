ebe55eb2416a2d9ba8d82bf3028f96bc152714fb
samnang <samnang.chhun@gmail.com>
2011-02-27 22:05:09 +0700

    remove unused stuff

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,14 +10,4 @@ module ApplicationHelper
       result << link_to(topic, "/people?topic=#{topic}", :class => 'topic-tag')
     end
   end
-
-  def edit_in_place(object, field, options={})
-    defaults = {:nil => "Click to edit #{field.to_s.gsub('_', ' ')}"}.merge(options)
-
-    if viewing_logged_in_user_profile?
-      best_in_place object, field, defaults
-    else
-      object.send(field)
-    end
-  end
 end
